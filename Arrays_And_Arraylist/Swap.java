710eacdf42c65f5fe84333fe8031c4e10e2f6a64
risus11 <shreyash6432@gmail.com>
2026-01-14 00:19:46 +0530

    swap code

---
+++ Arrays_And_Arraylist/Swap.java
@@ -0,0 +1,18 @@
+// package Arrays_And_Arraylist;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(arr));
+        swap(arr,2,3);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr,int index1,int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
